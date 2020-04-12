7f869e915e3eb272102aa892b45bea33e42c6eac
billystride <63526679+billystride@users.noreply.github.com>
2020-04-12 18:20:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -73,28 +73,7 @@ client.on('message', message => {
 
     }
 
-    if (message.content ==="!helpme") {
-        const exampleEmbed = new Discord.MessageEmbed()
-    	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-    	.setAuthor('Billy' 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-    	.setDescription('Some description here')
-    	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	    .addFields(
-    		{ name: 'Regular field title', value: 'Some value here' },
-    		{ name: '\u200B', value: '\u200B' },
-    		{ name: 'Inline field title', value: 'Some value here', inline: true },
-    		{ name: 'Inline field title', value: 'Some value here', inline: true },
-            	)
-    	.addField('Inline field title', 'Some value here', true)
-    	.setImage('https://i.imgur.com/wSTFkRM.png')
-    	.setTimestamp()
-	    .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
-        channel.send(exampleEmbed);
-        
-       }
+ 
 
     if (message.content.startsWith('!n00b')) {
         if (message.mentions.members.size === 1) {
