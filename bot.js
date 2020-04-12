8b7899f4bd21b06011548c70ae73c15cc90c6826
billystride <63526679+billystride@users.noreply.github.com>
2020-04-12 18:51:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -108,7 +108,7 @@ client.on('message', message => {
 	.setTimestamp()
 	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
 
-channel.message.send(exampleEmbed);   
+	message.channel.send(exampleEmbed);   
         
     }
     
